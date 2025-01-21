8c9018d8113efa05ca65e52c465ccf1bd8f6863f
Roman Kosovnenko <roman.kosovnenko@kmon.net>
2025-01-21 17:34:58 +0100

    feat: ensure data is read on application startup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import { DataService } from "./services/dataService";
 
 dotenv.config();
 
 const app = express();
+DataService.getInstance();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response): void => {
